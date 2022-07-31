b8547b23595558cdab2da83ca57de13a4bcb8ddd
Allgud <allgud83@gmail.com>
2022-07-31 14:29:57 +0300

    first screen

---
+++ src/lib/templates.js
@@ -0,0 +1,58 @@
+const levelTemplate = {
+    tag: "div",
+    classNames: ["level__choice"],
+    content: [
+        {
+            tag: "h2",
+            classNames: ["level__title"],
+            content: "Выбери сложность"
+        },
+        {
+            tag: "ul",
+            classNames: ["level__list"],
+            content: [
+                {
+                    tag: "li",
+                    classNames: ["level__item"],
+                    content: [
+                        {
+                            tag: "button",
+                            classNames: ["level__button"],
+                            attrs: {"data-value": "1"},
+                            content: 1
+                        }
+                    ]
+                },
+                {
+                    tag: "li",
+                    classNames: ["level__item"],
+                    content: [
+                        {
+                            tag: "button",
+                            classNames: ["level__button"],
+                            attrs: {"data-value": "2"},
+                            content: 2
+                        }
+                    ]
+                },
+                {
+                    tag: "li",
+                    classNames: ["level__item"],
+                    content: [
+                        {
+                            tag: "button",
+                            classNames: ["level__button"],
+                            attrs: {"data-value": "3"},
+                            content: 3
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            tag: "button",
+            classNames: ["btn", "choice__btn"],
+            content: "Старт"
+        }
+    ]
+}
