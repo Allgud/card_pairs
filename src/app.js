ecf29746f933244332239705900f85fe8f60c7ab
Allgud <allgud83@gmail.com>
2022-08-24 19:53:10 +0300

    add prettierrc and prettierignore

--- src/app.js
+++ src/app.js
@@ -1,32 +1,31 @@
 const app = document.querySelector('.app')
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
     application.renderScreen(app, levelTemplate, levelConfig)
 })
 
 const gameConfig = {
-    fn: [() => {}] // Пока пустая функция, чтобы ошибки в консоль не летели
+    fn: [() => {}], // Пока пустая функция, чтобы ошибки в консоль не летели
 }
 
 const levelConfig = {
-    fn: [levelListener]
+    fn: [levelListener],
 }
 
 function levelListener() {
     const levelList = document.querySelector('.level__list')
     const startBtn = document.querySelector('.btn')
-    
 
-    levelList.addEventListener('click', ({target}) => {
-        levelList.childNodes.forEach(el => {
+    levelList.addEventListener('click', ({ target }) => {
+        levelList.childNodes.forEach((el) => {
             el.classList.remove('active')
         })
         target.parentNode.classList.add('active')
         application.level = target.dataset.value
     })
 
-    startBtn.addEventListener("click", () => {
+    startBtn.addEventListener('click', () => {
         application.renderScreen(app, gameTemplate, gameConfig)
-        console.log(application.level);
+        console.log(application.level)
     })
 }
--- src/lib/templates.js
+++ src/lib/templates.js
@@ -1,71 +1,70 @@
 const levelTemplate = {
-    tag: "div",
-    classNames: ["level__choice"],
+    tag: 'div',
+    classNames: ['level__choice'],
     content: [
         {
-            tag: "h2",
-            classNames: ["level__title"],
-            content: "Выбери сложность"
+            tag: 'h2',
+            classNames: ['level__title'],
+            content: 'Выбери сложность',
         },
         {
-            tag: "ul",
-            classNames: ["level__list"],
+            tag: 'ul',
+            classNames: ['level__list'],
             content: [
                 {
-                    tag: "li",
-                    classNames: ["level__item"],
+                    tag: 'li',
+                    classNames: ['level__item'],
                     content: [
                         {
-                            tag: "button",
-                            classNames: ["level__button"],
-                            attrs: {"data-value": "1"},
-                            content: 1
-                        }
-                    ]
+                            tag: 'button',
+                            classNames: ['level__button'],
+                            attrs: { 'data-value': '1' },
+                            content: 1,
+                        },
+                    ],
                 },
                 {
-                    tag: "li",
-                    classNames: ["level__item"],
+                    tag: 'li',
+                    classNames: ['level__item'],
                     content: [
                         {
-                            tag: "button",
-                            classNames: ["level__button"],
-                            attrs: {"data-value": "2"},
-                            content: 2
-                        }
-                    ]
+                            tag: 'button',
+                            classNames: ['level__button'],
+                            attrs: { 'data-value': '2' },
+                            content: 2,
+                        },
+                    ],
                 },
                 {
-                    tag: "li",
-                    classNames: ["level__item"],
+                    tag: 'li',
+                    classNames: ['level__item'],
                     content: [
                         {
-                            tag: "button",
-                            classNames: ["level__button"],
-                            attrs: {"data-value": "3"},
-                            content: 3
-                        }
-                    ]
-                }
-            ]
+                            tag: 'button',
+                            classNames: ['level__button'],
+                            attrs: { 'data-value': '3' },
+                            content: 3,
+                        },
+                    ],
+                },
+            ],
         },
         {
-            tag: "button",
-            classNames: ["btn", "choice__btn"],
-            content: "Старт"
-        }
-    ]
+            tag: 'button',
+            classNames: ['btn', 'choice__btn'],
+            content: 'Старт',
+        },
+    ],
 }
 
-
 const gameTemplate = {
-    tag: "div",
-    classNames: ["game__screen"],
+    tag: 'div',
+    classNames: ['game__screen'],
     content: [
         {
-            tag: "div",
-            classNames: ["screen__header"],
-            content: "Игровой экран"
-        }
-    ]
+            tag: 'div',
+            classNames: ['screen__header'],
+            content: 'Игровой экран',
+        },
+    ],
 }
